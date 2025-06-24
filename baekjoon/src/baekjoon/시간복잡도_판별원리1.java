37052f8f76a2e5ceeaa7c30c3466c4c6cf339d09
osegyoung <osk0816@naver.com>
2025-06-24 23:48:07 +0900

    백준 문제 "첫 커밋"

---
+++ baekjoon/src/baekjoon/시간복잡도_판별원리1.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class 시간복잡도_판별원리1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	int N = 1000;
+	int cnt = 0;
+	for(int i=0; i<N; i++ ) {
+		  System.out.println( "연산 횟수:" +cnt++);
+	}
+	}
+}
+
+
